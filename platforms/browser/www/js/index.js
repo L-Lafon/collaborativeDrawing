b8f8635a9399d43794dccd1c3a0cbbe3a76079c7
SoleneM <so.malassigne@gmail.com>
2017-12-20 15:13:04 +0100

    Dessin de ligne, début de rectangles

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -42,5 +24,67 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
+var color = "black";
 app.initialize();
+
+window.onload = function() {
+
+    var clickX = new Array();
+    var clickY = new Array();
+    var clickDrag = new Array();
+    
+    var paint;
+
+    context =  document.getElementById('zoneDessin').getContext("2d");
+    canvas= document.getElementById('zoneDessin');
+    $('#zoneDessin').mousedown(function(e){
+      var mouseX = e.pageX - this.offsetLeft;
+      var mouseY = e.pageY - this.offsetTop;
+            
+      paint = true;
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+      redraw();
+    });
+
+    $('#zoneDessin').mousemove(function(e){
+      if(paint){
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+        redraw();
+      }
+    });
+
+    $('#zoneDessin').mouseup(function(e){
+      paint = false;
+    });
+
+    $('#zoneDessin').mouseleave(function(e){
+      paint = false;
+    });
+
+    function addClick(x, y, dragging)
+    {
+      clickX.push(x);
+      clickY.push(y);
+      clickDrag.push(dragging);
+    }
+
+    function redraw(){
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+      
+      context.strokeStyle = color;
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+                
+      for(var i=0; i < clickX.length; i++) {        
+        context.beginPath();
+        if(clickDrag[i] && i){
+          context.moveTo(clickX[i-1], clickY[i-1]);
+         }else{
+           context.moveTo(clickX[i]-1, clickY[i]);
+         }
+         context.lineTo(clickX[i], clickY[i]);
+         context.closePath();
+         context.stroke();
+        }
+    }
+}
--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -43,4 +25,111 @@ var app = {
     }
 };
 
+
+var curColor = "black";
+var clickColor = new Array();
+
+// Si on utilise un pinceau, ou alors rectangle/cercle
+var rect;
+var circle;
+var pencil;
 app.initialize();
+
+function changeColor(newColor){
+    curColor=newColor;
+}
+
+function useRect() {
+    rect = true;
+    circle = false;
+    pencil = false;
+    $("#inputRect").html(
+        "<input type=\"text\" id=\"hight\" placeholder=\"Higth\">"+
+        "<input type=\"text\" id=\"width\" placeholder=\"Width\">");
+}
+
+window.onload = function() {
+
+    // Array qui stockent les positions
+    var clickX = new Array();
+    var clickY = new Array();
+    var clickDrag = new Array();
+
+    // Variable qui permet de savoir si le trait doit être fait ou non
+    var paint;
+
+    context =  document.getElementById('zoneDessin').getContext("2d");
+    canvas= document.getElementById('zoneDessin');
+
+    // Si on souhaite dessiner un rectangle
+    if (rect) {
+        $('#zoneDessin').mousedown(function(e){
+            var mouseX = e.pageX - this.offsetLeft;
+            var mouseY = e.pageY - this.offsetTop;
+            
+            paint = true;
+            addRectClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            redraw();
+        })
+    }
+
+
+    // Si on utilise le pinceau
+    else {
+        $('#zoneDessin').mousedown(function(e){
+            var mouseX = e.pageX - this.offsetLeft;
+            var mouseY = e.pageY - this.offsetTop;
+                
+            paint = true;
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            redraw();
+        });
+
+        $('#zoneDessin').mousemove(function(e){
+            if(paint){
+                addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+                redraw();
+            }
+        });
+
+        $('#zoneDessin').mouseup(function(e){
+            paint = false;
+        });
+
+        $('#zoneDessin').mouseleave(function(e){
+            paint = false;
+        });
+    }
+
+
+    function addClick(x, y, dragging)
+    {
+      clickX.push(x);
+      clickY.push(y);
+      clickDrag.push(dragging);
+      clickColor.push(curColor);
+    }
+
+    function redraw(){
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+      
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+                
+      for(var i=0; i < clickX.length; i++) {        
+        context.beginPath();
+        if(clickDrag[i] && i){
+          context.moveTo(clickX[i-1], clickY[i-1]);
+         }else{
+           context.moveTo(clickX[i]-1, clickY[i]);
+         }
+         context.lineTo(clickX[i], clickY[i]);
+         context.closePath();
+         context.strokeStyle = clickColor[i];
+         context.stroke();
+        }
+    }
+
+    
+}
+
