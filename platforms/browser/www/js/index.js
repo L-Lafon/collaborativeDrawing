6f6ce708461d725daa7966d64d797281d987a8d8
Léa Lafon <lea.lafon@gmx.fr>
2017-12-27 11:45:55 +0100

    mise en forme

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -24,54 +24,95 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-var color = "black";
+
+
+var curColor = "black";
+var clickColor = new Array();
+
+// Si on utilise un pinceau, ou alors rectangle/cercle
+var rect;
+var circle;
+var pencil;
 app.initialize();
+
+function changeColor(newColor){
+    curColor=newColor;
+}
+
+function useRect() {
+    rect = true;
+    circle = false;
+    pencil = false;
+    $("#inputRect").html(
+        "<input type=\"text\" id=\"hight\" placeholder=\"Higth\">"+
+        "<input type=\"text\" id=\"width\" placeholder=\"Width\">");
+}
 
 window.onload = function() {
 
+    // Array qui stockent les positions
     var clickX = new Array();
     var clickY = new Array();
     var clickDrag = new Array();
-    
+
+    // Variable qui permet de savoir si le trait doit être fait ou non
     var paint;
 
     context =  document.getElementById('zoneDessin').getContext("2d");
     canvas= document.getElementById('zoneDessin');
-    $('#zoneDessin').mousedown(function(e){
-      var mouseX = e.pageX - this.offsetLeft;
-      var mouseY = e.pageY - this.offsetTop;
+
+    // Si on souhaite dessiner un rectangle
+    if (rect) {
+        $('#zoneDessin').mousedown(function(e){
+            var mouseX = e.pageX - this.offsetLeft;
+            var mouseY = e.pageY - this.offsetTop;
             
-      paint = true;
-      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-      redraw();
-    });
+            paint = true;
+            addRectClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            redraw();
+        })
+    }
 
-    $('#zoneDessin').mousemove(function(e){
-      if(paint){
-        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-        redraw();
-      }
-    });
 
-    $('#zoneDessin').mouseup(function(e){
-      paint = false;
-    });
+    // Si on utilise le pinceau
+    else {
+        $('#zoneDessin').mousedown(function(e){
+            var mouseX = e.pageX - this.offsetLeft;
+            var mouseY = e.pageY - this.offsetTop;
+                
+            paint = true;
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            redraw();
+        });
 
-    $('#zoneDessin').mouseleave(function(e){
-      paint = false;
-    });
+        $('#zoneDessin').mousemove(function(e){
+            if(paint){
+                addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+                redraw();
+            }
+        });
+
+        $('#zoneDessin').mouseup(function(e){
+            paint = false;
+        });
+
+        $('#zoneDessin').mouseleave(function(e){
+            paint = false;
+        });
+    }
+
 
     function addClick(x, y, dragging)
     {
       clickX.push(x);
       clickY.push(y);
       clickDrag.push(dragging);
+      clickColor.push(curColor);
     }
 
     function redraw(){
       context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
       
-      context.strokeStyle = color;
       context.lineJoin = "round";
       context.lineWidth = 5;
                 
@@ -84,7 +125,11 @@ window.onload = function() {
          }
          context.lineTo(clickX[i], clickY[i]);
          context.closePath();
+         context.strokeStyle = clickColor[i];
          context.stroke();
         }
     }
+
+    
 }
+
